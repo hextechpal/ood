310627222e0a4136470cd2ef559c888a66aa7d90
Prashant Pal <pal.prashant31@gmail.com>
2026-03-24 10:35:55 +0530

    elevator: small changes

--- src/elevator/main.rs
+++ src/elevator/main.rs
@@ -13,42 +13,42 @@ async fn main() {
     let c = Controller::new(2);
 
     let _ = c.request(0, 3).await;
-    tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
+    // tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
 
     let _ = c.request(0, 2).await;
-    tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
+    // tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
     info!("State : {:?}", c.state().await);
 
     let _ = c.request(3, 8).await;
-    tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
+    // tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
     info!("State : {:?}", c.state().await);
 
     let _ = c.request(8, 4).await;
-    tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
+    // tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
     info!("State : {:?}", c.state().await);
 
     let _ = c.request(4, 0).await;
-    tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
+    // tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
     info!("State : {:?}", c.state().await);
 
     let _ = c.request(4, 6).await;
-    tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
+    // tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
     info!("State : {:?}", c.state().await);
 
     let _ = c.request(6, 9).await;
-    tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
+    // tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
     info!("State : {:?}", c.state().await);
 
     let _ = c.request(9, 2).await;
-    tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
+    // tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
     info!("State : {:?}", c.state().await);
 
     let _ = c.request(2, 5).await;
-    tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
+    // tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
     info!("State : {:?}", c.state().await);
 
     let _ = c.request(5, 1).await;
-    tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
+    // tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
     info!("State : {:?}", c.state().await);
 
     let _ = c.request(8, 5).await;
